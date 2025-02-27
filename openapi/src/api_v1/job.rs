8ca9fe8bcbdeb74a422dfe95c07bc722aa1d1795
Axemc <192119741@qq.com>
2025-02-28 04:24:31 +0800

    get admin job ok

--- openapi/src/api_v1/job.rs
+++ openapi/src/api_v1/job.rs
@@ -1,3 +1,4 @@
+pub mod admin;
 pub mod job_get;
 pub mod job_list;
 pub mod zone_list;
---
+++ openapi/src/api_v1/job/admin/job_get.rs
@@ -0,0 +1,64 @@
+use openapi_common::define::{AsyncResponseFn, BaseRequest, BaseResponse, HttpFn, RequestFn};
+use openapi_model::job::job::AdminJobInfo;
+use reqwest::{Method, Response};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+#[serde(default)]
+pub struct AdminJobGetRequest {
+    pub job_id: String,
+}
+
+impl AdminJobGetRequest {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn with_job_id(mut self, job_id: String) -> Self {
+        self.job_id = job_id;
+        self
+    }
+
+    pub fn build(self) -> HttpFn<BaseResponse<AdminJobGetResponse>> {
+        Box::new(move || {
+            let request_fn: RequestFn = Box::new(move || BaseRequest {
+                method: Method::GET,
+                uri: format!("/admin/jobs/{}", self.job_id),
+                ..Default::default()
+            });
+            let response_fn: AsyncResponseFn<BaseResponse<AdminJobGetResponse>> =
+                Box::new(|response: Response| Box::pin(async move { Ok(response.json().await?) }));
+            (request_fn, response_fn)
+        })
+    }
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+#[serde(default)]
+pub struct AdminJobGetResponse {
+    #[serde(flatten)]
+    pub job_info: AdminJobInfo,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use openapi_common::client::OpenApiClient;
+    use openapi_common::client::config::OpenApiConfig;
+    use tracing::info;
+
+    #[tokio::test]
+    async fn test_job_get() -> anyhow::Result<()> {
+        tracing_subscriber::fmt::init();
+        dotenvy::dotenv()?;
+        let config = OpenApiConfig::new().load_from_env()?.builder();
+        let mut client = OpenApiClient::new(config);
+
+        let http_fn = AdminJobGetRequest::new()
+            .with_job_id("5p6nwsYQWaw".to_string())
+            .build();
+        let response = client.send(http_fn).await?;
+        info!("response: {:#?}", response);
+        Ok(())
+    }
+}
